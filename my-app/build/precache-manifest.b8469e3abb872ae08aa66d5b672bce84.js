75cbe7c047de24554875c3630aea2e53f66d2fa8
rafamorini <rvieira@ufl.edu>
2020-04-04 17:45:44 +0000

    final step

---
+++ my-app/build/precache-manifest.b8469e3abb872ae08aa66d5b672bce84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae6e47dbd8bbcf59c583fd3d353608a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efcf7ba0896b41246166",
+    "url": "/static/css/main.8b1517ce.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efcf7ba0896b41246166",
+    "url": "/static/js/main.2b75211f.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5241ca515a48b5f9be06809b80ebb506",
+    "url": "/static/media/OKAY.5241ca51.png"
+  }
+]);
